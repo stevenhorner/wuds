3769fb5d7046fb685264d92a1045f3f761ac4996
Steven Horner <steven@stevenhorner.com>
2015-10-03 19:54:47 +0100

    Updatenode-red logging URL format

--- config.py
+++ config.py
@@ -87,4 +87,4 @@ PUSHBULLET_DEVICE_IDEN = ''
 #=======================
 
 # (STR) URL to send detected devices to
-URL_LOGGING_SITE = 'http://ipaddress:port/wifi?bssid=%s&rssi=%s&essid=%s&oui=%s'
+URL_LOGGING_SITE = 'http://ipaddress:port/wifi?bssid=%s&rssi=%s&essid=%s'

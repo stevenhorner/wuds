28815753b542e41f68773a17ce5485222368b55d
Steven Horner <steven@stevenhorner.com>
2015-10-06 12:58:46 +0100

    Fixed spaces problem and screen logging
    Fixed crashing when AP name contains spaces. Added printing to screen to enable debugging better, some will need removing later.

--- core.py
+++ core.py
@@ -52,7 +52,9 @@ def log_probe(bssid, rssi, essid):
 
 def log_nodered(timestamp, bssid, rssi, essid):
     print ("Time: %s BSSID: %s RSSI: %s ESSID: %s " % (timestamp, bssid, rssi, essid))
-    res = urllib2.urlopen(URL_LOGGING_SITE % (bssid, rssi, essid))
+    print URL_LOGGING_SITE % (bssid, rssi, essid)
+    node_red_url = URL_LOGGING_SITE % (bssid, rssi, essid.replace(" ", "%20"))
+    res = urllib2.urlopen(URL_LOGGING_SITE % (node_red_url))
 
 def is_admin_oui(mac):
     return int(mac.split(':')[0], 16) & 2

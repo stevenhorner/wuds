b85ebb2593213da959cc08505e64e614660fc0fb
Steven Horner <steven@stevenhorner.com>
2015-10-23 21:35:21 +0100

    Add wuds name to node-red logging

--- core.py
+++ core.py
@@ -51,9 +51,10 @@ def log_probe(bssid, rssi, essid):
     log(1, (bssid, rssi, essid, oui))
 
 def log_nodered(timestamp, bssid, rssi, essid):
-    print ("Time: %s BSSID: %s RSSI: %s ESSID: %s " % (timestamp, bssid, rssi, essid))
-    # print URL_LOGGING_SITE % (bssid, rssi, essid)
-    node_red_url = URL_LOGGING_SITE % (bssid, rssi, essid.replace(" ", "%20"), WUDS_NAME)
+    wuds = WUDS_NAME
+    print ("Time: %s BSSID: %s RSSI: %s ESSID: %s " % (timestamp, bssid, rssi, essid, wuds))
+    # print URL_LOGGING_SITE % (bssid, rssi, essid, wuds)
+    node_red_url = URL_LOGGING_SITE % (bssid, rssi, essid.replace(" ", "%20"), wuds)
     res = urllib2.urlopen(node_red_url)
 
 def is_admin_oui(mac):
